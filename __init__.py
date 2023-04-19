38595d867f6fce0d3b1e6a482a6db647fcf53933
BtbN <btbn@btbn.de>
2023-04-19 22:41:36 +0200

    API call function

--- __init__.py
+++ __init__.py
@@ -5,8 +5,33 @@ from aqt.qt import *
 import requests
 
 
+WK_API_BASE="https://api.wanikani.com/v2"
+WK_REV="20170710"
+
+config = mw.addonManager.getConfig(__name__)
+
+
+def wk_api_req(ep):
+    api_key = config["WK_API_KEY"]
+    if not api_key:
+        raise Exception("No API Key!")
+
+    headers = {
+        "Authorization": "Bearer " + api_key,
+        "Wanikani-Revision": WK_REV
+    }
+    
+    res = requests.get(f"{WK_API_BASE}/{ep}", headers=headers)
+    res.raise_for_status()
+    return res.json()
+
+
 def do_sync():
-    pass
+    if not config["WK_API_KEY"]:
+        showInfo("Configure your WaniKani API key first.")
+        return
+
+    showInfo(repr(wk_api_req("user")))
 
 
 menu = QMenu("WKSync", mw)
