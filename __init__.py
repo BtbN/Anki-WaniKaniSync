eafa52494b55912da5b59cf4dc0a091234fbb6e5
BtbN <btbn@btbn.de>
2023-04-20 16:49:51 +0200

    Minimal assignments sync

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-from aqt import mw
+from aqt import mw, gui_hooks
 from aqt.utils import showInfo, qconnect
 from aqt.qt import *
 
 import requests
+from datetime import datetime
 
 
 WK_API_BASE="https://api.wanikani.com/v2"
@@ -20,10 +21,22 @@ def wk_api_req(ep):
         "Authorization": "Bearer " + api_key,
         "Wanikani-Revision": WK_REV
     }
-    
+
     res = requests.get(f"{WK_API_BASE}/{ep}", headers=headers)
     res.raise_for_status()
-    return res.json()
+    data = res.json()
+
+    if "object" in data and data["object"] == "collection":
+        next_url = data["pages"]["next_url"]
+        while next_url:
+            res = requests.get(next_url, headers=headers)
+            res.raise_for_status()
+            new_data = res.json()
+
+            data["data"] += new_data["data"]
+            next_url = new_data["pages"]["next_url"]
+
+    return data
 
 
 def do_sync():
@@ -31,7 +44,25 @@ def do_sync():
         showInfo("Configure your WaniKani API key first.")
         return
 
-    showInfo(repr(wk_api_req("user")))
+    user_data = wk_api_req("user")
+    sync_all = config["SYNC_ALL"]
+    granted_lvl = user_data["data"]["subscription"]["max_level_granted"]
+
+    if sync_all:
+        pass
+    else:
+        req = "assignments?unlocked=true&hidden=false"
+        if config["_LAST_ASSIGNMENTS_SYNC"]:
+            req += "&updated_after=" + config["_LAST_ASSIGNMENTS_SYNC"]
+        assignments = wk_api_req(req)
+
+
+
+    mw.addonManager.writeConfig(__name__, config)
+
+
+def analyze_answer(card, ease):
+    pass
 
 
 menu = QMenu("WKSync", mw)
@@ -40,3 +71,6 @@ mw.form.menuTools.addMenu(menu)
 sync_action = QAction("Sync", mw)
 qconnect(sync_action.triggered, do_sync)
 menu.addAction(sync_action)
+
+
+gui_hooks.reviewer_did_answer_card.append(analyze_answer)
