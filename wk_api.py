d8959bb9259291f12d4cd1f8782f068f33347367
BtbN <btbn@btbn.de>
2024-01-10 15:59:43 +0100

    Add timeout for requests

--- wk_api.py
+++ wk_api.py
@@ -13,7 +13,7 @@ limiter = Limiter(RequestRate(50, Duration.MINUTE))
 session = requests.Session()
 session.mount("https://", HTTPAdapter(max_retries=Retry(total=5, backoff_factor=0.5)))
 
-def wk_api_req(ep, full=True, data=None, put=False):
+def wk_api_req(ep, full=True, data=None, put=False, timeout=5):
     config = mw.addonManager.getConfig(__name__)
     api_key = config["WK_API_KEY"]
     if not api_key:
@@ -27,11 +27,11 @@ def wk_api_req(ep, full=True, data=None, put=False):
     with limiter.ratelimit(api_key, delay=True):
         if data is not None:
             if put:
-                res = session.put(f"{WK_API_BASE}/{ep}", headers=headers, json=data)
+                res = session.put(f"{WK_API_BASE}/{ep}", headers=headers, json=data, timeout=timeout)
             else:
-                res = session.post(f"{WK_API_BASE}/{ep}", headers=headers, json=data)
+                res = session.post(f"{WK_API_BASE}/{ep}", headers=headers, json=data, timeout=timeout)
         else:
-            res = session.get(f"{WK_API_BASE}/{ep}", headers=headers)
+            res = session.get(f"{WK_API_BASE}/{ep}", headers=headers, timeout=timeout)
     res.raise_for_status()
     data = res.json()
 
@@ -39,7 +39,7 @@ def wk_api_req(ep, full=True, data=None, put=False):
         next_url = data["pages"]["next_url"]
         while next_url:
             with limiter.ratelimit(api_key, delay=True):
-                res = session.get(next_url, headers=headers)
+                res = session.get(next_url, headers=headers, timeout=timeout)
             res.raise_for_status()
             new_data = res.json()
 
